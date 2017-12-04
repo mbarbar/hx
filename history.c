91f9db5a07ea29ee7c4850b65883e0de7e3c803f
Mohamad Barbar <mbarbar@runbox.com>
2017-12-04 17:43:07 +1100

    Implement history: init, print, free.

--- history.c
+++ history.c
@@ -7,9 +7,21 @@
 #include "history.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 struct history_list* history_list_init(void) {
-	return NULL;
+	struct history_list* list = malloc(sizeof(struct history_list));
+
+	if (list == NULL) {
+		perror("Unable to allocate size for struct history_list");
+		exit(1);
+	}
+
+	list->head = NULL;
+	list->tail = NULL;
+	list->curr = NULL;
+
+	return list;
 }
 
 void history_add(struct history_list* list, char* string) {
@@ -18,11 +30,30 @@ void history_add(struct history_list* list, char* string) {
 }
 
 void history_free(struct history_list* list) {
-	(void)list;
+	struct history_entry* curr = list->head;
+	while (curr != NULL) {
+		struct history_entry* prev = curr;
+		curr = curr->next;
+		free(prev);
+	}
+
+	free(list);
 }
 
 void history_print(struct history_list* list) {
-	(void)list;
+	struct history_entry* node = list->head;
+
+	fprintf(stderr, "History: ");
+
+	if (node == NULL) fprintf(stderr, "empty history");
+
+	while (node != NULL) {
+		fprintf(stderr, "(%s) -> ", node->string);
+		node = node->next;
+		if (node == NULL) fprintf(stderr, "END\n");
+	}
+
+	fprintf(stderr, "\n");
 }
 
 void history_move(struct history_list* list, int direction) {
